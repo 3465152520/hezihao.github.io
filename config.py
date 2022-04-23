b81128fa133e6081d9cf01a4d173d78d85067a77
hzh <3465152520@qq.com>
2022-04-23 17:16:29 +0800

    修改项目结构

--- config.py
+++ config.py
@@ -1,15 +1,18 @@
 import os
 from dotenv import load_dotenv
 
-basedir = os.path.abspath(os.path.dirname(__file__))    # 获取路径
+basedir = os.path.abspath(os.path.dirname(__file__))  # 获取路径
 load_dotenv(os.path.join(basedir, '.env'), encoding='utf-8')
 
-print('aa', basedir)
-print('bb', os.path.dirname(__file__))
+# aa = os.getenv('FLASK_DEBUG')
+# bb = os.getenv('FLASK_APP')
+# print('aa', aa)
+# print('bb', bb)
 
 
 class Config(object):
     pass
+    # DEBUG = os.getenv('FLASK_DEBUG')
 
 
 if __name__ == '__main__':
